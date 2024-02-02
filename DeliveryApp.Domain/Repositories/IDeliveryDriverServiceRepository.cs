6240c02961b65ed6162310be16615e4a38c6e372
Francisco Silva <compjrce@gmail.com>
2024-02-02 10:08:27 -0300

    Add DeliveryDriver in repository interface

---
+++ DeliveryApp.Domain/Repositories/IDeliveryDriverServiceRepository.cs
@@ -0,0 +1,10 @@
+using DeliveryApp.Domain.Entities;
+
+namespace DeliveryApp.Domain.Repositories;
+
+public interface IDeliveryDriverServiceRepository
+{
+    Task InsertAsync(DeliveryDriver deliveryDriver);
+
+    Task UpdateDriverLicenseImageAsync(string driverLicenseNumber, byte[] driverLicenseImage);
+}
